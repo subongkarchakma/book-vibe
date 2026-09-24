0e5235343b876b6b434e4464450522e306699215
subongkarchakmama <subongkar.chakma@gmail.com>
2026-09-24 08:07:37 +0600

    Added home page link in Navbar using Link component

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -6,16 +6,19 @@ const Navbar = () => {
   const links = (
     <>
       <li>
-        <Link href= "/books">Books</Link>
+        <Link href="/">Home</Link>
       </li>
       <li>
-       <Link href= "/listedBooks">Listed Books</Link>
+        <Link href="/books">Books</Link>
+      </li>
+      <li>
+        <Link href="/listedBooks">Listed Books</Link>
       </li>
     </>
   );
 
   return (
-    <div className="navbar bg-base-100 shadow-sm">
+    <div className="navbar bg-base-100 shadow-sm container mx-auto rounded-lg">
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -40,11 +43,13 @@ const Navbar = () => {
             tabIndex={-1}
             className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
           >
-           {links}
+            {links}
           </ul>
         </div>
-        <a className="btn btn-ghost text-xl font-bold">Book Vibes</a>
         <Image src={logoImg} alt="books" width={32} height={32} />
+        <Link className="btn btn-ghost text-xl font-bold" href="/">
+          Book Vibes
+        </Link>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">{links}</ul>
